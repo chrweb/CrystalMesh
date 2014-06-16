b54a903ca38a59bcff4506411b0b910e17015385
Christoph <w-baer@gmx.de>
2014-06-16 22:39:21 +0200

    ADDED: Metadata for crystal mesh lib

---
+++ CrystalMeshLib/Debug/Math/Geometry Vector3D.d
@@ -0,0 +1,6 @@
+Math/Geometry\ Vector3D.d: ../Math/Geometry\ Vector3D.cpp \
+ ../Math/Geometry.h ../Math/Geometry\ Vector3D.h
+
+../Math/Geometry.h:
+
+../Math/Geometry\ Vector3D.h:
---
+++ CrystalMeshLib/Math/Geometry Vector3D.h
@@ -0,0 +1,47 @@
+/*
+ * Geometry Vector3D.h
+ *
+ *  Created on: 14.06.2014
+ *      Author: christoph
+ */
+
+#ifndef GEOMETRY_VECTOR3D_H_
+#define GEOMETRY_VECTOR3D_H_
+
+namespace CrystalMesh{
+
+	namespace Math{
+
+		namespace Geometry{
+
+			struct Vector3D;
+			struct Vector2D;
+
+			Vector3D const operator+ (Vector3D const & aLeftOp, Vector3D const & aRightOp);
+
+			Vector3D const operator- (Vector3D const & aLeftOp, Vector3D const & aRightOp);
+
+			Vector3D const vectorFromXYZ( double const & aX, double const & aY, double const & aZ);
+
+			Vector3D const vectorFromXY0( double const & aX, double const & aY);
+
+			Vector3D const VectorFromXY0(Vector2D const & aVec);
+
+			Vector3D const crossProductOf(Vector3D const & a0, Vector3D const & a1);
+
+			double const dotProductOf(Vector3D const & a0, Vector3D const & a1);
+
+		}
+
+
+	}
+
+
+}
+
+
+
+
+
+
+#endif /* GEOMETRY_VECTOR3D_H_ */
