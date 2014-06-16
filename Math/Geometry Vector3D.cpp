4589f9e9d029b4f7afcf9484f465687965ad4268
Christoph <w-baer@gmx.de>
2014-06-16 21:57:39 +0200

    ADDED: staic members

--- Math/Geometry Vector3D.cpp
+++ Math/Geometry Vector3D.cpp
@@ -9,6 +9,24 @@
 
 using namespace CrystalMesh::Math::Geometry;
 
+/**
+ * statics
+ */
+
+// May different platforms have different descriptions fir NaN?
+#include <math.h>
+#define CM_NAN NAN
+
+Vector3D const Vector3D::NaN = {CM_NAN, CM_NAN, CM_NAN};
+Vector3D const Vector3D::zero = { 0.0, 0.0, 0.0};
+Vector3D const Vector3D::xAxis = {1.0, 0.0, 0.0};
+Vector3D const Vector3D::yAxis = {0.0, 1.0, 0.0};
+Vector3D const Vector3D::zAxis = {0.0, 0.0, 1.0};
+
+
+
+
+
 Vector3D const operator+ (Vector3D const & aLeftOp, Vector3D const & aRightOp){
 
 	Vector3D result = {
