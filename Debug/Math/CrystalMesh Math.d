5fd046fd3077f7bdb0b07c0260dc1cb6f1feb33d
Christoph <w-baer@gmx.de>
2014-06-16 22:14:01 +0200

    DELETED: eclipse project setings

--- Debug/Math/CrystalMesh Math.d
+++
@@ -1,9 +0,0 @@
-Math/CrystalMesh\ Math.d: ../Math/CrystalMesh\ Math.cpp \
- ../Math/CrystalMesh\ Math.h ../Math/Geometry.h \
- ../Math/Geometry\ Vector3D.h
-
-../Math/CrystalMesh\ Math.h:
-
-../Math/Geometry.h:
-
-../Math/Geometry\ Vector3D.h:
--- Debug/Math/Geometry Vector3D.d
+++
@@ -1,6 +0,0 @@
-Math/Geometry\ Vector3D.d: ../Math/Geometry\ Vector3D.cpp \
- ../Math/Geometry.h ../Math/Geometry\ Vector3D.h
-
-../Math/Geometry.h:
-
-../Math/Geometry\ Vector3D.h:
