f4df4def66fa2e05a7fb033baa7db56dba3b625b
Christoph <w-baer@gmx.de>
2014-08-07 22:53:01 +0200

    ADDED: Geometry stuff

--- CrystalMeshLib/Debug/Math/CrystalMesh Math.d
+++ CrystalMeshLib/Debug/Math/CrystalMesh Math.d
@@ -1,9 +1,9 @@
 Math/CrystalMesh\ Math.d: ../Math/CrystalMesh\ Math.cpp \
  ../Math/CrystalMesh\ Math.h ../Math/Geometry.h \
- ../Math/Geometry\ Vector3D.h
+ ../Math/Geometry\ Point3D.h
 
 ../Math/CrystalMesh\ Math.h:
 
 ../Math/Geometry.h:
 
-../Math/Geometry\ Vector3D.h:
+../Math/Geometry\ Point3D.h:
--- CrystalMeshLib/Debug/Math/Geometry Vector3D.d
+++ CrystalMeshLib/Debug/Math/Geometry Vector3D.d
@@ -1,6 +1,6 @@
 Math/Geometry\ Vector3D.d: ../Math/Geometry\ Vector3D.cpp \
- ../Math/Geometry.h ../Math/Geometry\ Vector3D.h
+ ../Math/Geometry.h ../Math/Geometry\ Point3D.h
 
 ../Math/Geometry.h:
 
-../Math/Geometry\ Vector3D.h:
+../Math/Geometry\ Point3D.h:
