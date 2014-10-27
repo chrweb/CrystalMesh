c528bdcb26154f68eafef0aa5ec89b928c317d7a
Christoph <w-baer@gmx.de>
2014-10-27 22:18:08 +0100

    continued with netbeans migration

---
+++ Mathbox/Geometry Vector2D.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   Geometry Vector2D.h
+ * Author: christoph
+ *
+ * Created on 27. Oktober 2014, 22:10
+ */
+
+#ifndef GEOMETRY_VECTOR2D_H
+#define	GEOMETRY_VECTOR2D_H
+
+
+
+#endif	/* GEOMETRY_VECTOR2D_H */
+
