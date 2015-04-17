4eacb46ecbc7273c27c17164dab125a0beabb046
Christoph <w-baer@gmx.de>
2015-04-17 20:30:46 +0200

    ...

--- CrystalMesh/DelaunayTest/TestTriangle.cpp
+++ CrystalMesh/DelaunayTest/TestTriangle.cpp
@@ -5,6 +5,7 @@ using namespace Mathbox;
 using namespace Geometry;
 using namespace Toolbox;
 using namespace Delaunay3;
+using namespace Subdiv3;
 
 namespace{
 
@@ -43,7 +44,32 @@ namespace{
             EXPECT_EQ(bnds[0]->getEnext(), bnds[1]);
             EXPECT_EQ(bnds[1]->getEnext(), bnds[2]);
             EXPECT_EQ(bnds[2]->getEnext(), bnds[0]);
-        
+            
+            //edges, edge rings:
+            for (FacetEdge* current: bnds){
+                DirectedEdgeRing * dring = current->getDirectedEdgeRing();
+                EXPECT_TRUE(dring!=nullptr);
+                EXPECT_TRUE(dring->getRingMember() == current);
+            }
+            
+            // vertices:
+            auto verts = aTri.getBoundaryVertices();
+            
+            //unique
+            auto vertsEnd = std::unique(verts.begin(), verts.end());
+            EXPECT_EQ(verts.end(), vertsEnd);
+            
+            //to adjancent edge rings
+            for (auto const&current : verts){
+                auto adj = getAdjacentFacetEdges(*current);
+                EXPECT_EQ(2, adj.size());
+            }
+            
+            //Domain:
+            Vertex* domain = aTri.mpDualEdgeRing->getOrg();
+            EXPECT_TRUE(domain != nullptr);
+            auto adjDual = getAdjacentRingsOf(*domain);
+            EXPECT_EQ(2, adjDual.size());
         }
 }
 
@@ -55,6 +81,7 @@ TEST_F(TriangleTester, construction){
     Triangle trig0 = mDT.makeTriangle(vertexDataOf(p0), vertexDataOf(p1), vertexDataOf(p2));
     SCOPED_TRACE("construction");
     validSingletonTrianlge(trig0);
+    validSingletonTrianlge(trig0.getCounterOriented());
     
 }
 
